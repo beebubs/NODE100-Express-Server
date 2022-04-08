0a5618fe6e058ca8dd44396d75824dbb53360a1a
Bernadette B <90285283+beebubs@users.noreply.github.com>
2022-04-07 19:31:17 -0700

    completed app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // import files and packages up here
-
+const data = require("../server/data.json")
+const express = require("express");
+const morgan = require("morgan");
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan("dev"));
+
+app.get("/", function(req, res) {
+    res.status(200).send("status 200");
+    
+});
+
+app.get("/data", function(req, res) {
+    res.json(data)
+});
 
 // finally export the express application
 module.exports = app;
